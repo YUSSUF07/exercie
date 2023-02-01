d74c9cf6829412ee2373e313a0c5749372aac6ef
Youssouf Faye <fayey@MATINF.uqtr.ca>
2023-02-01 17:33:24 -0500

    solution2

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -4,43 +4,19 @@ class Program
 {
     public static void Main(string[] args)
     {
-        deviner(23,1,100);
-        
+        deviner(23, 1, 100);
+
     }
 
-    public static void deviner(int x, int inf, int sup) 
+    public static void deviner(int x, int inf, int sup)
     {
-        /*
-                do
-                {
-                  int milieu = (inf + sup)/2;
-                    Console.Write("Est ce que le nombre est superieur a" +milieu );
-                    if(milieu == x)
-                        {
-                        Console.WriteLine("le chiffre a deviner est " + x);
 
-                        }
-                    else if (x >milieu)
-                    {
-                        Console.WriteLine("OUI");
-                        inf= milieu + 1;
-                    }
-                    else
-                    {
-                        Console.WriteLine("NON");
-                        sup = milieu - 1;
-
-                    }
-                } 
-                while (inf!=sup);
-        */
-
-        int milieu = (inf + sup) / 2; ;
-       do
+        do
         {
-            milieu = (inf + sup) / 2;
+            int milieu = (inf + sup) / 2;
             Console.Write("Est ce que le nombre est superieur a " + milieu);
-            if (milieu < x){
+            if (x > milieu)
+            {
                 Console.WriteLine(" OUI");
                 inf = milieu + 1;
             }
@@ -48,21 +24,44 @@ class Program
             {
                 Console.WriteLine(" NON");
                 sup = milieu ;
+
             }
             
-
         }
-        while (milieu != x && inf < sup);
-        if (milieu == x)
-        {
-            Console.WriteLine("le chiffre a deviner est " + x);
+        while (inf != sup);
+        Console.WriteLine("le nombre a deviner est " + x);
+        /* 
 
-        }
-        else
-        {
-            Console.WriteLine("le chiffre a deviner n existe pas ");
+       int milieu = (inf + sup) / 2; 
+      do
+       {
+           milieu = (inf + sup) / 2;
+           Console.Write("Est ce que le nombre est superieur a " + milieu);
+           if (milieu < x){
+               Console.WriteLine(" OUI");
+               inf = milieu + 1;
+           }
+           else
+           {
+               Console.WriteLine(" NON");
+               sup = milieu ;
+           }
 
-        }
+
+       }
+       while (milieu != x && inf < sup);
+       if (milieu == x)
+       {
+           Console.WriteLine("le chiffre a deviner est " + x);
+
+       }
+       else
+       {
+           Console.WriteLine("le chiffre a deviner n existe pas ");
+
+       }
+
+      } */
 
     }
 }
